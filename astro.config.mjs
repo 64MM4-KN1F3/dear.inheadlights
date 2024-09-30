cd21fd5da61a72bf7dd7c973ae77880f29f814ba
64 <64mm4-kn1f3@gmail.com>
2024-09-30 19:46:00 +1000

    Try adding new-tab links from https://dan.salvagni.io/b/astro-plugin-open-external-links-in-new-tab/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { targetBlank } from './src/plugins/targetBlank';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://inheadlights.com'
+  site: 'https://inheadlights.com',
+  markdown: {
+    rehypePlugins: [[targetBlank, { domain: 'inheadlights.com' }]],
+  },
 });
